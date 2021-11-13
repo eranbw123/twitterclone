43c7f1a95dd2d6d1c315460da9b23b69b7aee451
eranbw123 <eranbw123@gmail.com>
2021-11-13 23:01:28 +0200

    completed submit form through vanilla javascript

--- tweets/models.py
+++ tweets/models.py
@@ -7,5 +7,8 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="image/", blank=True, null=True)
 
+    class Meta:
+        ordering = ["-id"]
+
     def serialize(self):
         return {"id": self.id, "content": self.content, "likes": random.randint(0, 200)}
