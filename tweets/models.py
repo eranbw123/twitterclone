00444a1e2f8135cbcc8879a10378da06fadfd8cc
eranbw123 <eranbw123@gmail.com>
2021-11-14 22:05:05 +0200

    cuztomized the admin page

--- tweets/models.py
+++ tweets/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 import random
 
+# from django.conf import settings
+from django.contrib.auth.models import User
+
+# User = settings.AUTH_USER_MODEL
+
 
 class Tweet(models.Model):
     # blank = required in django, null = required in the DataBase
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # many users can many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="image/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.content} ({self.id})"
 
     class Meta:
         ordering = ["-id"]
