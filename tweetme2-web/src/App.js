6537f243e6ba4a3115d2f1b23a1a4e4cfa5c5e77
eranbw123 <eranbw123@gmail.com>
2021-11-20 03:58:00 +0200

    started writing to react

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,7 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const loadTweets = (tweetsElement) => {
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "api/tweets";
+  const responseType = "json";
+  xhr.responseType = responseType;
+  xhr.open(method, url);
+  xhr.onload = () => {
+    const serverResponse = xhr.response;
+    const listedItems = serverResponse;
+    var finalTweetStr = "";
+    var i;
+    for (i = 0; i < listedItems.length; i++) {
+      var tweetObj = listedItems[i];
+      finalTweetStr += formatTweetElement(tweetObj);
+    }
+    tweetsElement.innerHTML = finalTweetStr;
+  };
+  xhr.send();
+};
+
+const App = () => {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    const tweetItems = [{ content: "test" }, { content: "hello world" }];
+    setTweets(tweetItems);
+  }, []);
+
+  console.log(tweets);
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +47,14 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          {tweets.map((tweet, index) => {
+            return <li>{tweet.content}</li>;
+          })}
+        </p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
