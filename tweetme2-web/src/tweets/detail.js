f30646c00bd0efb3035908854ba165dcba39c0c4
eranbw123 <eranbw123@gmail.com>
2021-12-11 05:08:18 +0200

    removed momentjs cutom time messages

--- tweetme2-web/src/tweets/detail.js
+++ tweetme2-web/src/tweets/detail.js
@@ -24,26 +24,6 @@ export const Tweet = (props) => {
   const className = props.className ? props.className : "tweet border";
   const { id, content, username } = props.tweet;
   var moment = require("moment");
-  moment.updateLocale("en", {
-    relativeTime: {
-      future: "in %s",
-      past: "%s ago",
-      s: "a few seconds",
-      ss: "%d seconds",
-      m: "a minute",
-      mm: "%d minutes",
-      h: "an hour",
-      hh: "%d hours",
-      d: "a day",
-      dd: "%d days",
-      w: "a week",
-      ww: "%d weeks",
-      M: "a month",
-      MM: "%d months",
-      y: "a year",
-      yy: "%d years",
-    },
-  });
   const timeAgo = moment(
     props.tweet.timestamp,
     "YYYY-MM-DD  HH:mm:ss:SSSSSS"
